ad38bccdc9e82f1bf31569bb11409a8a90c9c74a
Armon Porter <max-student@maxtrain.com>
2020-02-07 15:53:28 -0500

    Add project files.

---
+++ Practice/Practice.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice {
+    class Major { //Class Major with properties id, description, and minsat
+        public int id;
+        public string description;
+        public int minsat;
+
+        public Major(int Id, string Description, int MinSAT) { //Method Major with input Id, Description, and MinSAT
+            id = Id;
+            description = Description;
+            minsat = MinSAT;
+            }//Major constructor
+        public void Print() { //Method Print to display properties
+            Console.WriteLine($"The {description} major has an ID of {id} and Minimum SAT Score of {minsat}.");
+            } //print method
+        } //class Major
+    class Student {
+        public string firstname;
+        public string lastname;
+        public int sat;
+        public double gpa;
+        public Major major; //must be an existing Major class
+        public int id;
+
+        public Student(int Id, string Firstname, string Lastname, int SAT, double GPA, Major Major) {
+            id = Id;
+            firstname = Firstname;
+            lastname = Lastname;
+            sat = SAT;
+            gpa = GPA;
+            major = Major;
+            }
+        public void Print() { //Method Print to display properties
+            Console.WriteLine($"Student {firstname} {lastname} has a major of {major.description}, an SAT Score of {sat}, and GPA of {gpa}.");
+            } //print method
+        }//Class Student
+    class Instructor {
+        public int id;
+        public string firstname;
+        public string lastname;
+        public int yrsexp;
+        public bool tenure;
+
+        public Instructor(int Id, string Firstname, string Lastname, int EXP, bool Tenure = false) {
+            id = Id;
+            firstname = Firstname;
+            lastname = Lastname;
+            yrsexp = EXP;
+            tenure = Tenure;
+            }
+        public void Print() { //Method Print to display properties
+            Console.WriteLine($"Instructor {firstname} {lastname} has {yrsexp} years of experience" + ((tenure == true) ? " and is tenured." : "."));
+            } //print method
+        }
+    class Course {
+        public int id;
+        public string description;
+        public int section;
+        public Instructor instructor;
+        public Course(int Id, string Description, int Section, Instructor Instructor) { //how can i set the instructor to null or TBD?
+            id = Id;
+            description = Description;
+            section = Section;
+            instructor = Instructor;
+            }
+        public void Print() { //Method Print to display properties
+            Console.WriteLine($"Course {description} is {section} and is taught by {instructor.lastname}.");
+            } //print method
+        } //end of course class
+
+    }//namespace Practice
---
+++ Practice/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Practice {
+    class Program {
+        static void Main(string[] args) {
+
+            var majors = new Major[] {
+                new Major(10, "Engineering", 1300),
+                new Major(20, "Accounting", 1000),
+                new Major(30, "Nursing", 1000),
+                new Major(40, "Math", 1300),
+                new Major(50, "Education", 1100)
+            };
+
+            for (var idx = 0; idx < majors.Length; idx++) {
+                var major = majors[idx];
+                major.Print();
+            }
+
+            var students = new Student[] {
+                new Student(10, "Ken", "Bone", 1500, 3.6, majors[0]),
+                new Student(11, "Kim", "Kessling", 1175, 3.4, majors[2]),
+                new Student(12, "Tracy", "Tusky", 1350, 3.5, majors[3]),
+                new Student(13, "Fred", "Flintstone", 600, 1.0, majors[1])
+            };
+
+            foreach (var student in students){
+                student.Print();
+            }
+
+            var Prof = new Instructor(10, "Prof", "Dolittle", 25);
+            //Prof.Print();
+        }
+    }
+}
